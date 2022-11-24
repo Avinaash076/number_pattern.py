e8d69aa9a0a4434c06ae44c7bc87f1b62cf5bfb2
Avinaash <116714314+Avinaash076@users.noreply.github.com>
2022-11-24 19:35:50 +0530

    Create number_pattern.py
    this is a python file ,which by using you can make a star rhombus pattern .Thank you for reading

---
+++ number_pattern.py
@@ -0,0 +1,29 @@
+for  i in range(1):
+    print("     *",end="")
+print()
+for  i in range(1):
+    print("   * * *",end="   ")
+print()
+for  i in range(1):
+    print("  * * * *",end="   ")
+print()
+for  i in range(1):
+    print(" * * * * *",end="  ")
+print()
+for  i in range(1):
+    print("* * * * * *",end= " ")
+print()
+for i in range(1):
+    print(" * * * * *",end = " ")
+print()
+for i in range(1):
+    print("  * * * *", end="   ")
+print()
+for i in range(1):
+    print("   * * *", end="   ")
+
+print()
+for i in range(1):
+    print("     *", end="")
+
+print()
